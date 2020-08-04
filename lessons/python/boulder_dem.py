a747a44036ecfeb61c19af421e514433491913ad
Mark Piper <mark.piper@colorado.edu>
2020-08-04 12:18:49 -0600

    Add Python example to be used for git lesson

---
+++ lessons/python/boulder_dem.py
@@ -0,0 +1,35 @@
+"""An example of reading topographical data from a file and displaying it."""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+topo_file = "../../data/boulder-topography.asc"
+
+
+def read():
+    try:
+        topo = pd.read_csv(topo_file, header=None)
+    except IOError:
+        print("IOError: file '{}' cannot be read".format(topo_file))
+    else:
+        return topo
+
+
+def display(data, show=False, outfile="boulder_dem.png"):
+    fig, ax = plt.subplots()
+    elev = ax.imshow(data, cmap="jet")
+    fig.colorbar(elev, label="Elevation (m)")
+    plt.title("Boulder Topography")
+
+    if show is True:
+        plt.show()
+    else:
+        plt.savefig(outfile, dpi=96)
+        plt.close()
+
+
+if __name__ == "__main__":
+    topo = read()
+    if topo is not None:
+        display(topo)
