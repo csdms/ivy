476023e5ea9360f21cbd830eed08128684a4154d
Rachel Allen <rachelallen@berkeley.edu>
2020-08-14 04:41:04 +0000

    Use jet colormap again for display

--- lessons/se/boulder_dem.py
+++ lessons/se/boulder_dem.py
@@ -18,7 +18,7 @@ def read():
 
 def display(data, show=False, outfile="boulder_dem.png"):
     fig, ax = plt.subplots()
-    elev = ax.imshow(data, cmap="viridis")
+    elev = ax.imshow(data, cmap="jet")
     fig.colorbar(elev, label="Elevation (m)")
     plt.title("Boulder Topography")
 
