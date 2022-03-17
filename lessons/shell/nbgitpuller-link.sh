5f788847a029f7e3c21d9935e0581fc1e0703dc8
Mark Piper <mark.piper@colorado.edu>
2022-03-17 16:35:54 -0600

    Add a helpful shell script

---
+++ lessons/shell/nbgitpuller-link.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+# Make links through the nbgitpuller-link CLI.
+
+HUB=https://lab.openearthscape.org
+REPO=https://github.com/csdms/ivy
+BRANCH=main
+FILE=lessons/pymt/index.ipynb
+
+nbgitpuller-link --version
+
+nbgitpuller-link \
+    --jupyterhub-url=$HUB \
+    --repository-url=$REPO \
+    --branch=$BRANCH \
+    --launch-path="$FILE" \
+    --interface="lab"
