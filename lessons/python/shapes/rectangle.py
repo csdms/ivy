fef2a0114dee2262456e507993dee87735df72df
Mark Piper <mark.piper@colorado.edu>
2023-07-26 15:50:59 -0600

    Simplify arguments for Rectangle and Square

--- lessons/python/shapes/rectangle.py
+++ lessons/python/shapes/rectangle.py
@@ -3,15 +3,14 @@ from .shape import Shape
 
 class Rectangle(Shape):
     def __init__(
-        self, lower_left: tuple = (1.0, 1.0), upper_right: tuple = (3.0, 2.0)
+        self, lower_left: tuple = (1.0, 1.0), width: float = 3.0, height: float = 2.0
     ) -> None:
         self.ll = lower_left
-        self.ur = upper_right
-        x = [self.ll[0], self.ur[0], self.ur[0], self.ll[0]]
-        y = [self.ll[1], self.ll[1], self.ur[1], self.ur[1]]
+        self.w = width
+        self.h = height
+        x = [self.ll[0], self.ll[0] + self.w, self.ll[0] + self.w, self.ll[0]]
+        y = [self.ll[1], self.ll[1], self.ll[1] + self.h, self.ll[1] + self.h]
         super().__init__(x, y)
 
     def calculate_area(self) -> None:
-        width = self.ur[0] - self.ll[0]
-        height = self.ur[1] - self.ll[1]
-        self.area = width * height
+        self.area = self.w * self.h
--- lessons/python/shapes/square.py
+++ lessons/python/shapes/square.py
@@ -2,8 +2,5 @@ from .rectangle import Rectangle
 
 
 class Square(Rectangle):
-    def __init__(
-        self, lower_left: tuple = (1.0, 1.0), side_length: float = 2.0
-    ) -> None:
-        upper_right = (lower_left[0] + side_length, lower_left[1] + side_length)
-        super().__init__(lower_left, upper_right)
+    def __init__(self, lower_left: tuple = (1.0, 1.0), width: float = 2.0) -> None:
+        super().__init__(lower_left, width, width)
