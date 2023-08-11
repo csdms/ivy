8f65caa2908125eaec64c524ccea47f4f23a3616
Mark Piper <mark.piper@colorado.edu>
2023-08-11 12:01:32 -0600

    Match code in solver module to functions notebook

--- lessons/python/ivy_diffusion/solver.py
+++ lessons/python/ivy_diffusion/solver.py
@@ -1,18 +1,29 @@
 """A solver for the one-dimensional diffusion equation."""
 import numpy as np
 
-np.set_printoptions(formatter={"float": "{: 6.1f}".format})
+np.set_printoptions(precision=1, floatmode="fixed")
 
 
-def solve1d(concentration, spacing=1.0, time_step=1.0, diffusivity=1.0):
+def calculate_time_step(grid_spacing, diffusivity):
+    return grid_spacing**2 / diffusivity / 2.1
+
+
+def set_initial_profile(domain_size=100, boundary_left=500, boundary_right=0):
+    concentration = np.empty(domain_size)
+    concentration[: int(domain_size / 2)] = boundary_left
+    concentration[int(domain_size / 2) :] = boundary_right
+    return concentration
+
+
+def solve1d(concentration, grid_spacing=1.0, time_step=1.0, diffusivity=1.0):
     """Solve the one-dimensional diffusion equation with fixed boundary conditions.
 
     Parameters
     ----------
     concentration : ndarray
         The quantity being diffused.
-    spacing : float (optional)
-        Grid spacing.
+    grid_spacing : float (optional)
+        Distance between grid nodes.
     time_step : float (optional)
         Time step.
     diffusivity : float (optional)
@@ -21,7 +32,7 @@ def solve1d(concentration, spacing=1.0, time_step=1.0, diffusivity=1.0):
     Returns
     -------
     result : ndarray
-        The temperatures after time *time_step*.
+        The concentration after a time step.
 
     Examples
     --------
@@ -32,30 +43,25 @@ def solve1d(concentration, spacing=1.0, time_step=1.0, diffusivity=1.0):
     >>> solve1d(z, diffusivity=0.25)
     array([   0.0,    1.2,    2.5,    1.2,    0.0])
     """
-    flux = -diffusivity * np.diff(concentration) / spacing
-    concentration[1:-1] -= time_step * np.diff(flux) / spacing
-
-    return concentration
+    flux = -diffusivity * np.diff(concentration) / grid_spacing
+    concentration[1:-1] -= time_step * np.diff(flux) / grid_spacing
 
 
-def _run_example():
-    """An example of running solve1d."""
-    print(_run_example.__doc__)
-    D = 100
-    Lx = 10
-    dx = 0.5
-    C1 = 500
-    C2 = 0
-    C = np.empty(Lx)
-    C[: int(Lx / 2)] = C1
-    C[int(Lx / 2) :] = C2
-    dt = dx * dx / D / 2.1
+def diffusion_example():
+    """An example of using `solve1d` in a diffusion problem."""
+    print(example.__doc__)
+    D = 100  # diffusivity
+    Lx = 10  # domain length
+    dx = 0.5  # grid spacing
+
+    dt = calculate_time_step(dx, D)
+    C = set_initial_profile(Lx)
+
     print("Time = 0\n", C)
-
     for t in range(1, 5):
         solve1d(C, dx, dt, D)
-        print(f"Time = {t*dt}\n", C)
+        print(f"Time = {t*dt:.4f}\n", C)
 
 
 if __name__ == "__main__":
-    _run_example()
+    diffusion_example()
